2914bb9eed19c0dada22dade0751f5796c7b60dc
elstarkov <sawr22@student.bth.se>
2023-04-24 19:36:47 +0200

    Added additional templates for card game. Another method added for CardGrahpic-class.

--- src/Controller/Card/CardGraphic.php
+++ src/Controller/Card/CardGraphic.php
@@ -38,6 +38,40 @@ class CardGraphic extends Card
         "[Q \xE2\x99\xA6]", "[K \xE2\x99\xA6]",
     ];
 
+    private $cardValues = [
+        "[A \xE2\x99\xA0]",
+        "[2 \xE2\x99\xA0]" => 2, "[3 \xE2\x99\xA0]"  => 3, 
+        "[4 \xE2\x99\xA0]"  => 4, "[5 \xE2\x99\xA0]"  => 5,
+        "[6 \xE2\x99\xA0]"  => 6, "[7 \xE2\x99\xA0]"  => 7,
+        "[8 \xE2\x99\xA0]"  => 8, "[9 \xE2\x99\xA0]"  => 9,
+        "[10 \xE2\x99\xA0]"  => 10, "[J \xE2\x99\xA0]"  => 11,
+        "[Q \xE2\x99\xA0]" => 12, "[K \xE2\x99\xA0]"  => 13,
+
+        "[A \xE2\x99\xA3]",
+        "[2 \xE2\x99\xA3]"  => 2, "[3 \xE2\x99\xA3]" => 3,
+        "[4 \xE2\x99\xA3]" => 4, "[5 \xE2\x99\xA3]" => 5,
+        "[6 \xE2\x99\xA3]" => 6, "[7 \xE2\x99\xA3]" => 7,
+        "[8 \xE2\x99\xA3]" => 8, "[9 \xE2\x99\xA3]" => 9,
+        "[10 \xE2\x99\xA3]" => 10, "[J \xE2\x99\xA3]" => 11,
+        "[Q \xE2\x99\xA3]" => 12, "[K \xE2\x99\xA3]" => 13,
+
+        "[A \xE2\x99\xA5]",
+        "[2 \xE2\x99\xA5]" => 2, "[3 \xE2\x99\xA5]" => 3,
+        "[4 \xE2\x99\xA5]" => 4, "[5 \xE2\x99\xA5]" => 5,
+        "[6 \xE2\x99\xA5]" => 6, "[7 \xE2\x99\xA5]" => 7,
+        "[8 \xE2\x99\xA5]" => 8, "[9 \xE2\x99\xA5]" => 9,
+        "[10 \xE2\x99\xA5]" => 11, "[J \xE2\x99\xA5]" => 11,
+        "[Q \xE2\x99\xA5]" => 12, "[K \xE2\x99\xA5]" => 13,
+
+        "[A \xE2\x99\xA6]",
+        "[2 \xE2\x99\xA6]" => 2, "[3 \xE2\x99\xA6]" => 3,
+        "[4 \xE2\x99\xA6]" => 4, "[5 \xE2\x99\xA6]" => 5,
+        "[6 \xE2\x99\xA6]" => 6, "[7 \xE2\x99\xA6]" => 7,
+        "[8 \xE2\x99\xA6]" => 8, "[9 \xE2\x99\xA6]" => 9,
+        "[10 \xE2\x99\xA6]" => 10, "[J \xE2\x99\xA6]" => 11,
+        "[Q \xE2\x99\xA6]" => 12, "[K \xE2\x99\xA6]" => 13,
+    ];
+
     public function __construct()
     {
         parent::__construct();
@@ -70,5 +104,15 @@ class CardGraphic extends Card
     public function getAsString(): string
     {
         return $this->deck[$this->value];
+    }
+
+    public function convertToPoints($card): int
+    {
+        foreach ($this->cardValues as $key => $value) {
+            if ($key === $card) {
+                return $value;
+            }
+        }
+        return 0;
     }
 }
--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,21 +2,144 @@
 
 namespace App\Controller;
 
-// use App\Card\CardGraphic;
-// use App\Card\CardHand;
-// use App\Card\DeckOfCards;
+use App\Card\CardGraphic;
+use App\Card\CardHand;
+use App\Card\DeckOfCards;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-//use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class GameController extends AbstractController
 {
-    #[Route("/game", name: "game")]
+    #[Route("/game", name: "game", methods: ['GET'])]
     public function game(): Response
     {
         return $this->render('game/game.html.twig');
+    }
+
+    #[Route("/game/new", name: "new_game", methods: ['GET'])]
+    public function NewGame(): Response
+    {
+        return $this->render('game/new.html.twig');
+    }
+
+    #[Route("/game", name: "game_post", methods: ['POST'])]
+    public function gamePost(
+        SessionInterface $session
+    ): Response
+    {
+
+        $deck = new DeckOfCards();
+
+        $bankHand = new CardHand();
+        $playerHand = new CardHand();
+
+        $session->set("playerPoints", 0);
+        $session->set("bankPoints", 0);
+
+        $session->set("playerHand", $playerHand);
+        $session->set("bankHand", $bankHand);
+
+        $session->set("deck", $deck);
+
+        return $this->redirectToRoute('game_play');
+    }
+
+    #[Route("/game/play", name: "game_play", methods: ['GET'])]
+    public function gamePlay(
+        SessionInterface $session
+    ): Response
+    {
+
+        $deck = $session->get("deck");
+        $playerHand = $session->get("playerHand");
+        $bankHand = $session->get("bankHand");
+
+        if ($deck->getRemain() == 0) {
+            $session->remove("deck");
+            $session->remove("playerHand");
+            $session->remove("bankHand");
+            $session->remove("playerPoints");
+            $session->remove("bankPoints");
+            return $this->redirectToRoute('new_game');
+        } else {
+            $data = [
+                "playerP" => $session->get("playerPoints"),
+                "bankP" => $session->get("bankPoints"),
+                "remain" => $deck->getRemain(),
+                "print" => $playerHand->printHand(),
+                "playerHand" =>$playerHand->getHand(),
+                "printBank" => $bankHand->printHand(),
+            ];
+
+            return $this->render('game/play.html.twig', $data);
+        }
+    }
+
+    #[Route("/game/play/draw", name: "draw_card", methods: ['POST'])]
+    public function drawCard(
+        SessionInterface $session
+    ): Response
+    {
+        $deck = $session->get("deck");
+        $playerHand = $session->get("playerHand");
+        $bankHand = $session->get("bankHand");
+        $playerPoints = $session->get("playerPoints");
+
+        while (true) {
+            $playerCard = new CardGraphic();
+            $playerCard->getRandCard();
+            $val = $playerCard->getAsString();
+
+            if (in_array($val, $playerHand->getHand())) {
+                continue;
+            } elseif (in_array($val, $bankHand->getHand())) {
+                continue;
+            } else {
+                $deck->modifyDeck($val);
+                $playerHand->add($val);
+                $pointsToAdd = $playerCard->convertToPoints($val);
+                $playerPoints += $pointsToAdd;
+                $session->set("playerPoints", $playerPoints);
+                break;
+            }
+        }
+
+        return $this->redirectToRoute('game_play');
+    }
+
+    #[Route("/game/play/stay", name: "stay", methods: ['POST'])]
+    public function stay(
+        SessionInterface $session
+    ): Response
+    {
+        $deck = $session->get("deck");
+        $bankHand = $session->get("bankHand");
+        $playerHand = $session->get("playerHand");
+        $bankPoints = $session->get("bankPoints");
+
+        while (true) {
+            $bankCard = new CardGraphic();
+            $bankCard->getRandCard();
+            $val = $bankCard->getAsString();
+
+            if (in_array($val, $bankHand->getHand())) {
+                continue;
+            } elseif (in_array($val, $playerHand->getHand())) {
+                continue;
+            } else {
+                $deck->modifyDeck($val);
+                $bankHand->add($val);
+                $pointsToAdd = $bankCard->convertToPoints($val);
+                $bankPoints += $pointsToAdd;
+                $session->set("bankPoints", $bankPoints);
+                break;
+            }
+        }
+
+        return $this->redirectToRoute('game_play');
     }
 
     #[Route("/game/doc", name: "doc")]
